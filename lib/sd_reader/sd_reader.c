3f916c4f93f7c6a5f4d6c18bcb3874479fd5abef
yami-five <konrad.rykala.94@gmail.com>
2025-02-17 22:41:15 +0100

    started implementing loading obj from sd card

--- lib/sd_reader/sd_reader.c
+++ lib/sd_reader/sd_reader.c
@@ -4,6 +4,32 @@
 FRESULT f_res;
 FATFS microSDFatFs;
 
+uint8_t readline(uint8_t *line, uint8_t length, FIL *file)
+{
+	uint br;
+	uint8_t lineBuffer[1];
+	f_read(file, lineBuffer, 1, &br);
+	if (lineBuffer[0] == EOF)
+		return 1;
+	line[0] = lineBuffer[0];
+	uint8_t counter = 1;
+	while ((lineBuffer[0] != '\n') && (counter < length - 1))
+	{
+		f_read(file, lineBuffer, 1, &br);
+		line[counter] = lineBuffer[0];
+		counter++;
+	}
+	return 0;
+}
+
+void clear_line(uint8_t *line, uint8_t length)
+{
+	for (uint8_t i = 0; i < length; i++)
+	{
+		line[i] = 0;
+	}
+}
+
 void sd_init()
 {
 	DEV_Digital_Write(SD_CS_PIN, 1);
@@ -20,5 +46,17 @@ void sd_init()
 	else
 	{
 		printf("SD card mount file system success!! \r\n");
+	}
+	FIL file;
+	f_open(&file, "cube.obj", FA_READ);
+
+	uint8_t linelength=32;
+	uint8_t line[linelength];
+	clear_line(&line, linelength);
+
+	while (!readline(&line, linelength, &file))
+	{
+		readline(&line, linelength, &file);
+		uint8_t test = 1;
 	}
 }
--- lib/sd_reader/sd_reader.h
+++ lib/sd_reader/sd_reader.h
@@ -1,1 +1,5 @@
+#include "ff.h"
+
+uint8_t readline(uint8_t * line,uint8_t length,FIL * file);
+void clear_line(uint8_t * line, uint8_t length);
 void sd_init();
